36531d3d1d03025d2a32b4a8838520c9e51756c7
Jair Otero <Joterobu@estudiente.ibero.edu.co>
2025-11-29 20:28:11 -0500

    HU1 - se incluye tabla para listar los usuarios

--- usuarios.php
+++ usuarios.php
@@ -1,7 +1,13 @@
 <?php
 $usuarios = json_decode(file_get_contents('data/usuarios.json'), true);
 echo "<h2>Lista de Usuarios</h2>";
+
+echo "<table border='1'>
+        <tr><th>ID</th><th>Nombre</th></tr>";
+
 foreach ($usuarios as $u) {
-    echo "<p>".$u['nombre']."</p>";
+    echo "<tr><td>{$u['id']}</td><td>{$u['nombre']}</td></tr>";
 }
+
+echo "</table>";
 ?>
